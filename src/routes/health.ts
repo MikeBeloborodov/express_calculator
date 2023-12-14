a0bbfca781ff9de3977f731d9a3d309aaebcd6f0
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2023-12-14 22:51:15 +0400

    added routing

---
+++ src/routes/health.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import express from "express";
+
+const router = Router();
+
+router.get("/", (req: express.Request, res: express.Response) => {
+  res.send({ message: "OK" });
+});
